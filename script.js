1cf287760b56109c682576ddeb35127dbd0e7553
sreelekha-2 <sreelekhakutikanti@gmail.com>
2022-07-04 17:39:15 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let str = 'My Name is Sreelekha';
 
-// function fstTask() {
-//   for (let i = 0; i < str.length; i++) {
-//     setTimeout(function () {
-//       console.log(str.slice(0, i + 1));
-//     }, i * 1000);
-//   }
-// }
-let out = '';
+function fstTask() {
+  for (let i = 0; i < str.length; i++) {
+    setTimeout(function () {
+      console.log(str.slice(0, i + 1));
+    }, i * 1000);
+  }
+}
+// let out = '';
 
-str.split('').forEach((ele) => console.log(ele + out));
+// str.split('').forEach((ele, index) => {
+//   setTimeout(function () {
+//     out += ele;
+//     console.log(out);
+//   }, index * 1000);
+// });
+
+// let res = '';
+
+// str.split('').forEach((ele, index) => {
+//   setTimeout(function () {
+//     console.log(str);
+//   }, index * 1000);
+// });
 
 function secTask() {
   let k = 0;
@@ -21,6 +34,6 @@ function secTask() {
   }
 }
 
-// fstTask();
+fstTask();
 
 setTimeout(secTask, str.length * 1000);
