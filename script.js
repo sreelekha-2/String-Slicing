f29d25c4937d7b2f08ab9b069e5b51f858b03701
sreelekha-2 <sreelekhakutikanti@gmail.com>
2022-07-04 18:30:42 +0530

    1 file modified

--- script.js
+++ script.js
@@ -7,30 +7,13 @@ function fstTask() {
     }, i * 1000);
   }
 }
-// let out = '';
-
-// str.split('').forEach((ele, index) => {
-//   setTimeout(function () {
-//     out += ele;
-//     console.log(out);
-//   }, index * 1000);
-// });
-
-// let res = '';
-
-// str.split('').forEach((ele, index) => {
-//   setTimeout(function () {
-//     console.log(str);
-//   }, index * 1000);
-// });
 
 function secTask() {
-  let k = 0;
-  for (let j = str.length; j > 0; j--) {
-    k = k + 1000;
+  for (let i = 0; i < str.length; i++) {
     setTimeout(function () {
+      let j = str.length - i;
       console.log(str.slice(0, j));
-    }, k + 1000);
+    }, i * 1000);
   }
 }
 
